432057151e1bc01679f7838a1f10bf35ee810700
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-15 19:09:43 -0500

    Refactored helper function for daylistItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,20 +8,20 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   })
 
-  function formatSpots (props, spotsRemaining){
+  function formatSpots (spotsRemaining){
     if(spotsRemaining === 1) {
-      return `${props} spot remaining`;
+      return `${spotsRemaining} spot remaining`;
     } else if (spotsRemaining === 0){
       return "no spots remaining"
     }
-    return `${props} spots remaining`;
+    return `${spotsRemaining} spots remaining`;
 
   }
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{formatSpots(props.spots, props.spots)}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
