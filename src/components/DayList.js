a69193184e002ff09ed26d243f04506c4f5bfb70
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-15 19:40:02 -0500

    Added Daylist component

---
+++ src/components/DayList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import DayListItem from 'components/DayListItem'
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+export default function DayList(props) {
+  const daysData = days.map(day => {
+    return <DayListItem
+    key={day.id}
+    name={day.name} 
+    spots={day.spots} 
+    selected={day.name === props.day}
+    setDay={props.setDay}  />
+  })
+  return <ul>{daysData}</ul>
+}
