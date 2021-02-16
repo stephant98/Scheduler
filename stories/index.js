5e3625933e216bb0e04ec49e5f5904b8b84e8cd1
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-16 18:29:42 -0500

    Added header and empty stories to Appointment

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,10 @@ import Button from "components/Button";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem"
 import InterviewerList from "components/InterviewerList"
+import Appointment from "components/Appointment"
+import Header from "components/Appointment/Header.js"
+import Empty from "components/Appointment/Empty.js"
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -131,6 +135,15 @@ storiesOf("Button", module)
             setInterviewer={action("setInterviewer")}
           />
         ));
+
+    storiesOf("Appointment", module)
+      .addParameters({
+       backgrounds: [{ name: "white", value: "#fff", default: true }]
+      })
+      .add("Appointment", () => <Appointment />)
+      .add("Appointment with Time", () => <Appointment time="12pm"/>)
+      .add("Header", () => <Header time="12pm"/>)
+      .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
 
 
 
