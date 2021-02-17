7001de24dfe60aaaaa161abab96a4763489b3186
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-17 14:45:53 -0500

    Added state to form component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show.js"
 import Confirm from "components/Appointment/Confirm.js"
 import Status from "components/Appointment/Status.js"
 import Error from "components/Appointment/Error.js"
+import Form from "components/Appointment/Form.js"
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,5 +153,6 @@ storiesOf("Button", module)
       .add("Status Saving", () => <Status message="Saving" />)
       .add("Status Deleting", () => <Status message="Deleting" />)
       .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
-
+      .add("Form Edit", () => <Form name="Stephan Truchsess" interviewers={interviewers} interviewer={3} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+      .add("Form Create", () => <Form interviewers={interviewers}  onSave={action("onSave")} onCancel={action("onCancel")}/>)
 
